a8b8d93c57411c100dc1c79c203c6eff4575b1a4
Nikhil Nalande <55831609+nalandenv@users.noreply.github.com>
2021-09-11 14:37:05 +0530

    VSS-100 header implement local storage on locale selector

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,23 @@
-console.log("connected");
+// local storage
+const contBtn = document.querySelector(".btn-continue");
+const closeBtn = document.querySelector(".exit");
+
+const hideLocaleSelector = () => {
+  const localeSelectorContainer = document.querySelector(
+    ".locale-selector-container"
+  );
+  localeSelectorContainer.style.display = "none";
+};
+
+closeBtn.addEventListener("click", () => {
+  hideLocaleSelector();
+});
+const country = document.querySelector("#country");
+const locSt = localStorage.getItem("country");
+if (locSt) {
+  hideLocaleSelector();
+}
+contBtn.addEventListener("click", () => {
+  localStorage.setItem("country", country.value);
+  hideLocaleSelector();
+});
